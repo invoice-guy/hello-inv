185e8b13acf766b5cf47679ca7e84fd89e455ce8
invoice-guy <chihoon82@hanmail.net>
2021-07-11 16:56:11 +0900

    initial commit

---
+++ src/main/java/io/spring/batch/entity/pay/PayRepository.java
@@ -0,0 +1,12 @@
+package io.spring.batch.entity.pay;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 9. 21.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+public interface PayRepository extends JpaRepository<Pay, Long> {
+}
